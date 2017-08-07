a39419fc26b2a7b1fa07df9134c0928e88cf37fa
Devin <devinj1121@gmail.com>
2017-08-07 14:48:48 -0500

    updates

--- cases/spiders/general.py
+++ cases/spiders/general.py
@@ -2,17 +2,16 @@ import scrapy
 
 url = input("Enter the starting page of results: ")
 page_count = input("Enter the amount of pages to scrape: ")
+links = []
 
-class BatchSpider(scrapy.Spider):
+class GeneralSpider(scrapy.Spider):
     name = "general"
     start_urls = []
 
     # Populate start_urls
-    for i in range page_count:
+    for i in range (int(page_count)):
         start_urls.append(url + "page-" + str(i))
 
     def parse(self, response):
-
-        next_page = response.css('li.next a::attr(href)').extract_first()
-        if next_page is not None:
-            yield response.follow(next_page, self.parse)
+        for link in response.css('.avh::attr(href)'):
+            links.append('https://rospravosudie.com/' + link.extract())
